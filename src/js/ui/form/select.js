a176f88bc336fd9b6e3eb81e1d031b3a072cfb8b
InFinity_54 <corentinkreicher@gmail.com>
2023-03-18 05:21:33 +0100

    Ajout d'éléments communs de l'UI (boutons simple, select, input text...)

---
+++ src/js/ui/form/select.js
@@ -0,0 +1,26 @@
+jQuery(".form_select").click((event) => {
+  const formSelect = jQuery(event.target);
+  const formSelectOptions = jQuery(event.target.parentNode.children[2]);
+
+  if (formSelect.hasClass("active")) {
+    formSelect.removeClass("active");
+    formSelectOptions.hide();
+  } else {
+    formSelect.addClass("active");
+    formSelectOptions.slideDown(175);
+  }
+});
+
+jQuery(".form_select_option").click((event) => {
+  const selectedOption = jQuery(event.target);
+  const selectedOptionDisplay = jQuery(event.target.parentNode.parentNode.children[1].children[0]);
+  const formSelect = jQuery(event.target.parentNode.parentNode.children[1]);
+  const formSelectOptions = jQuery(event.target.parentNode.parentNode.children[2]);
+
+  selectedOptionDisplay.html(selectedOption.html());
+  selectedOptionDisplay.attr("data-value", selectedOption.data("value"));
+  formSelectOptions.hide();
+  formSelectOptions.find('div.form_select_option').removeClass("selected");
+  formSelect.removeClass("active");
+  selectedOption.addClass("selected");
+});
